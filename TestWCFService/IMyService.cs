24b26ad8d36295918093c5fccd2e57910871e652
jigarkumartailor <saijht@hotmail.com>
2020-07-08 13:37:17 -0500

    Test WCF

---
+++ TestWCFService/IMyService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestWCFService
+{
+    [ServiceContract]
+    interface IMyService
+    {
+        [OperationContract]
+        string GetString();
+
+        [OperationContract]
+        string GetMessage(string name);
+
+        [OperationContract]
+        string GetResult(int SId, string Name, int m1, int m2, int m3);
+        [OperationContract]
+        int MaxArraynumber(int[] arr);
+
+        [OperationContract]
+        int[] sortedArray(int[] arr);
+
+    }
+}
---
+++ TestWCFService/MyService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestWCFService
+{
+    class MyService : IMyService
+    {
+        public string GetMessage(string name)
+        {
+            return "Mr." + name;
+        }
+
+        public string GetResult(int SId, string Name, int m1, int m2, int m3)
+        {
+            double Avg = (m1 + m2 + m3) / 3;
+            if (Avg > 35)
+            {
+                return "Pass";
+            }
+            else
+            {
+                return "Fail";
+            }
+             
+        }
+
+        public string GetString()
+        {
+            return "Jigar Tailor";
+        }
+
+        public int MaxArraynumber(int[] arr)
+        {
+            int Max = arr[0];
+            for (int i = 0; i < arr.Length-1; i++)
+            {
+                if (Max < arr[i])
+                {
+                    Max = arr[i];
+                }
+            }
+            return Max;
+        }
+
+        public int[] sortedArray(int[] arr)
+        {
+            Array.Sort(arr);
+            return arr;
+
+        }
+    }
+}
